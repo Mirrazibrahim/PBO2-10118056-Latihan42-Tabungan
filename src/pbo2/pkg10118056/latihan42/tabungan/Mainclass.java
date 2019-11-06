1ff163195baa94d757474d5ebc3c30ea19ff2ba7
Mirraz Ibrahim <55575435+Mirrazibrahim@users.noreply.github.com>
2019-11-06 23:48:26 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118056/latihan42/tabungan/Mainclass.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118056.latihan42.tabungan;
+
+/**
+ *
+ * @author 
+ * Nama : Mirraz Ibrahim
+ * Nim : 10118056
+ * Kelas: IF-2
+ */
+public class Mainclass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("=====Program Penarikan Uang=====");
+        Tabungan tbg = new Tabungan(0);
+        tbg.ambilUang(0);
+    }
+    
+}
